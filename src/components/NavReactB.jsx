2381bd1465e53b3d9689c43cfb9671ee92748f42
Fabio <fabioramosnic@gmail.com>
2021-05-09 17:49:22 -0300

    Primeras pruebas

---
+++ src/components/NavReactB.jsx
@@ -0,0 +1,12 @@
+import { Navbar, Container } from 'react-bootstrap';
+
+
+export default function NavReactB() {
+    return (
+        <Navbar expand="lg" variant="dark" bg="dark">
+            <Container>
+                <Navbar.Brand href="#">Navbar</Navbar.Brand>
+            </Container>
+        </Navbar>
+    );
+}
