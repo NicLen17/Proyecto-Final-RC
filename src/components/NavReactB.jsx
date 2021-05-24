2d6e7460185281f340b0dec376d491ada5396df9
lrlichardi <lrlichardi@hotmail.com>
2021-05-24 20:14:24 -0300

    modificaciones nav bar y footer

--- src/components/NavReactB.jsx
+++ src/components/NavReactB.jsx
@@ -1,12 +1,22 @@
-import { Navbar, Container } from 'react-bootstrap';
-
+import {Nav, Navbar, Container } from 'react-bootstrap';
+import './NavReactB.css';
 
 export default function NavReactB() {
     return (
-        <Navbar expand="lg" variant="dark" bg="dark">
-            <Container>
-                <Navbar.Brand href="#">Navbar</Navbar.Brand>
-            </Container>
-        </Navbar>
+        <Navbar bg="light" expand="lg" className="d-flex">
+        <Navbar.Brand href="#home"><img className="img-logo" src="https://www.logaster.com.es/blog/wp-content/uploads/sites/4/2019/03/0106_t_phone-pixels-logo_2.png"/></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
+          <Nav className="mr-auto linknav">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#link">Sobre nosotros</Nav.Link>
+            <Nav.Link href="#link">Contacto</Nav.Link>
+            <Nav.Link href="#link">Log In</Nav.Link>
+            <Nav.Link href="#link">Registrarse</Nav.Link>
+
+            
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
 }
