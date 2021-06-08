44a3a17c6e8a6a9673cb1028f9b864700172d174
Mauro <maurogrezzana@gmail.com>
2021-06-08 09:37:24 -0300

    mas detalles al contacto

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
-import Carro from './components/Carro';
+import "./App.css";
+import Carro from "./components/Carro";
 import NavReactB from "./components/NavReactB";
-import SCards from './components/SCards';
-import Spublicidad from './components/Spublicidad';
-import Sabout from './components/Sabout';
-import Footer from './components/Footer';
-import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
-import { useState } from 'react';
-import Productos from './components/Pages/Productos';
-import Contacto from './components/Contacto';
-import Admin from './components/Pages/Admin';
+import SCards from "./components/SCards";
+import Spublicidad from "./components/Spublicidad";
+import Sabout from "./components/Sabout";
+import Footer from "./components/Footer";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import { useState } from "react";
+import Productos from "./components/Pages/Productos";
+import Contacto from "./components/Contacto";
+
+import Admin from "./components/Pages/Admin";
 
 function App() {
   return (
     <div className="App">
-
       <Router>
         <NavReactB />
         <Switch>
@@ -29,22 +34,17 @@ function App() {
             <Sabout />
           </Route>
 
-
           <Route path="/productos" exact>
             <Productos />
           </Route>
           <Route path="/contacto" exact>
             <Contacto />
           </Route>
-          <Route path="/carrito" exact>
-
-          </Route>
+          <Route path="/carrito" exact></Route>
         </Switch>
       </Router>
 
       <Footer />
-
-
     </div>
   );
 }
