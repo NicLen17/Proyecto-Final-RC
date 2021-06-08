f39a3f4f8048734abc95fb3e10065f520c02aedc
Fabio <fabioramosnic@gmail.com>
2021-06-08 12:23:23 -0300

    Nuevo diseño de cards

--- src/components/SCards.jsx
+++ src/components/SCards.jsx
@@ -1,7 +1,7 @@
 import { Card } from 'react-bootstrap'
 import React from 'react'
 import './SCards.css'
-import {Tabs, Tab} from 'react-bootstrap'
+import { Tabs, Tab } from 'react-bootstrap'
 
 
 export default function Cards() {
@@ -13,100 +13,145 @@ export default function Cards() {
             <div>
                 <Tabs fill variant="tabs" className="navproductos" defaultActiveKey="profile" id="uncontrolled-tab-example">
                     <Tab className="tabss" eventKey="home" title="Tablets">
-                    <div className="flexcard">
-                    <Card className="cards">
-                    <Card.Img className="cardimg" variant="top" src="https://d500.epimg.net/cincodias/imagenes/2020/01/09/tablets/1578563297_555928_1578563341_noticia_normal.jpg" />
-                    <Card.Body className="cardfooter">
-                        <p>$$$</p>
-                        <p>---</p>
-                    </Card.Body>
-                </Card>
-                <Card className="cards">
-                    <Card.Img className="cardimg" variant="top" src="https://img.global.news.samsung.com/ar/wp-content/uploads/2019/08/Product-Image-Galaxy-Tab-S6-2.jpg" />
-                    <Card.Body className="cardfooter">
-                        <p>$$$</p>
-                        <p>---</p>
-                    </Card.Body>
-                </Card>
-                <Card className="cards">
-                    <Card.Img className="cardimg" variant="top" src="https://i.blogs.es/175e48/galaxy-tab-e/1366_2000.jpg" />
-                    <Card.Body className="cardfooter">
-                        <p>$$$</p>
-                        <p>---</p>
-                    </Card.Body>
-                </Card>
-                <Card className="cards">
-                    <Card.Img className="cardimg" variant="top" src="https://images.samsung.com/is/image/samsung/assets/uy/p6_gro2/p6_initial_pf/tablets/All_PF_Smartphones_720x800_Tab_S7_header_1st.jpg?$ORIGIN_JPG$" />
-                    <Card.Body className="cardfooter">
-                        <p>$$$</p>
-                        <p>---</p>
-                    </Card.Body>
-                </Card>
-            </div>
+                        <div className="flexcard">
+                            <div className="cardprincipal">
+                                <div className="cardimg">
+                                    <img className="imgsa" src="https://topesdegama.com/app/uploads-topesdegama.com/2021/03/AperTablets.jpg" alt="" />
+                                </div>
+                                <div className="contenidocard">
+                                    <h1>Precio</h1>
+                                    <ul>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div className="cardprincipal">
+                                <div className="cardimg">
+                                    <img src="https://images-na.ssl-images-amazon.com/images/I/61%2Bwysvj4dL._AC_SX425_.jpg" alt="" />
+                                </div>
+                                <div className="contenidocard">
+                                    <h1>Precio</h1>
+                                    <ul>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="cardprincipal">
+                                <div className="cardimg">
+                                    <img src="https://s1.eestatic.com/2020/09/03/elandroidelibre/el_androide_libre_517961105_180359768_1200x845.jpg" alt="" />
+                                </div>
+                                <div className="contenidocard">
+                                    <h1>Precio</h1>
+                                    <ul>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
                     </Tab>
                     <Tab className="tabss" eventKey="profile" title="Celulares">
-                    <div className="flexcard">
-                    <Card className="cards">
-                    <Card.Img className="cardimg" variant="top" src="https://images.samsung.com/is/image/samsung/latin-galaxy-s20-plus-g985-bts-sm-g985fzpjgto-frontbpurple-thumb-261222598" />
-                    <Card.Body className="cardfooter">
-                        <p>$$$</p>
-                        <p>---</p>
-                    </Card.Body>
-                </Card>
-                <Card className="cards">
-                    <Card.Img className="cardimg" variant="top" src="https://images.fravega.com/f300/6471af0b982f4f6dd159ae0cdad2a820.jpg" />
-                    <Card.Body className="cardfooter">
-                        <p>$$$</p>
-                        <p>---</p>
-                    </Card.Body>
-                </Card>
-                <Card className="cards">
-                    <Card.Img className="cardimg" variant="top" src="https://images-na.ssl-images-amazon.com/images/I/81AT%2BFlc%2BEL._AC_SX679_.jpg" />
-                    <Card.Body className="cardfooter">
-                        <p>$$$</p>
-                        <p>---</p>
-                    </Card.Body>
-                </Card>
-                <Card className="cards">
-                    <Card.Img className="cardimg" variant="top" src="https://images.samsung.com/is/image/samsung/p6pim/pe/sm-g780fzglltp/gallery/pe-galaxy-s20-fe-g780-357343-sm-g780fzglltp-thumb-327368871?$LazyLoad_Home_IMG$" />
-                    <Card.Body className="cardfooter">
-                        <p>$$$</p>
-                        <p>---</p>
-                    </Card.Body>
-                </Card>
-            </div>
+                        <div className="flexcard">
+                            <div className="cardprincipal">
+                                <div className="cardimg">
+                                    <img className="imgsa" src="https://images.samsung.com/is/image/samsung/p6pim/pe/sm-g780fzglltp/gallery/pe-galaxy-s20-fe-g780-357343-sm-g780fzglltp-thumb-327368871?$LazyLoad_Home_IMG$" alt="" />
+                                </div>
+                                <div className="contenidocard">
+                                    <h1>Precio</h1>
+                                    <ul>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div className="cardprincipal">
+                                <div className="cardimg">
+                                    <img src="https://cdn.tmobile.com/content/dam/t-mobile/ntm/devices/samsung/a71/fg/fg-ss-Galaxy-A71-5G-Black-750x750.png" alt="" />
+                                </div>
+                                <div className="contenidocard">
+                                    <h1>Precio</h1>
+                                    <ul>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="cardprincipal">
+                                <div className="cardimg">
+                                    <img src="https://ss7.vzw.com/is/image/VerizonWireless/apple-iphone-12-64gb-purple-53017-mjn13ll-a?$device-lg$" alt="" />
+                                </div>
+                                <div className="contenidocard">
+                                    <h1>Precio</h1>
+                                    <ul>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
                     </Tab>
                     <Tab className="tabss" eventKey="contact" title="Accesorios">
-                    <div className="flexcard">
-                <Card className="cards">
-                    <Card.Img className="cardimg" variant="top" src="https://www.tuexperto.com/wp-content/uploads/2019/02/portada-e1550832415514.jpg" />
-                    <Card.Body className="cardfooter">
-                        <p>$$$</p>
-                        <p>---</p>
-                    </Card.Body>
-                </Card>
-                <Card className="cards">
-                    <Card.Img className="cardimg" variant="top" src="https://static2.abc.es/media/201205/05/S-Pebble-interior--478x270.jpg" />
-                    <Card.Body className="cardfooter">
-                        <p>$$$</p>
-                        <p>---</p>
-                    </Card.Body>
-                </Card>
-                <Card className="cards">
-                    <Card.Img className="cardimg" variant="top" src="https://larepublica.pe/resizer/PUH6NejjguwCGzDkKJ-ClR7SxR0=/1250x735/top/smart/cloudfront-us-east-1.images.arcpublishing.com/gruporepublica/AXD2IKU6JVHFNJLF4OYH4GVLIY.png" />
-                    <Card.Body className="cardfooter">
-                        <p>$$$</p>
-                        <p>---</p>
-                    </Card.Body>
-                </Card>
-                <Card className="cards">
-                    <Card.Img className="cardimg" variant="top" src="https://www.todoaccesoriosmoviles.com/wp-content/uploads/2019/12/auriculares-samsung-s7.jpg" />
-                    <Card.Body className="cardfooter">
-                        <p>$$$</p>
-                        <p>---</p>
-                    </Card.Body>
-                </Card>
-            </div>
+                        <div className="flexcard">
+                            <div className="cardprincipal">
+                                <div className="cardimg">
+                                    <img className="imgsa" src="https://lh3.googleusercontent.com/proxy/4FuND69wPkDcXXwZC4Uf512Q2z0ejhhG6-XZN4iiPhNiZgmUrpc-5eaOl5vgWL1x4PMLgiCaGLHY-cayCvQAGNi2SP7kFT2wvU4MgH2Ahinx8jf75cVGL-D3YAWqxezxBM2b" alt="" />
+                                </div>
+                                <div className="contenidocard">
+                                    <h1>Precio</h1>
+                                    <ul>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div className="cardprincipal">
+                                <div className="cardimg">
+                                    <img src="https://zonaphone.com.co/wp-content/uploads/2020/05/tienda-online-zonaphone-balacas-airpods-manos-libres-compra-por-internet-cambio-de-display-servicio-tecnico-repuestos-accesorios-celulares-samsung-iphone-android-apple-cargadores-cables-blister-case.png" alt="" />
+                                </div>
+                                <div className="contenidocard">
+                                    <h1>Precio</h1>
+                                    <ul>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="cardprincipal">
+                                <div className="cardimg">
+                                    <img src="https://tienda.claro.com.ar/wcsstore/Claro/images/catalog/productos/646x1000/7003415.jpg" alt="" />
+                                </div>
+                                <div className="contenidocard">
+                                    <h1>Precio</h1>
+                                    <ul>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
                     </Tab>
                 </Tabs>
             </div>
