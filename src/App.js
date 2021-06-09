e11c4e1c8fdacf0d162be214dd8f1fb894279218
conymejuto <conymejuto@gmail.com>
2021-06-08 23:30:02 -0300

    cambios app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Carro from "./components/Carro";
 import NavReactB from "./components/NavReactB";
-<<<<<<< HEAD
 import {
   BrowserRouter as Router,
   // Redirect,
@@ -53,51 +52,6 @@ export default function App() {
       <Sabout />
 
       {/* <Footer/>  */}
-=======
-import SCards from "./components/SCards";
-import Spublicidad from "./components/Spublicidad";
-import Sabout from "./components/Sabout";
-import Footer from "./components/Footer";
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Route,
-  Switch,
-} from "react-router-dom";
-import { useState } from "react";
-import Productos from "./components/Pages/Productos";
-import Contacto from "./components/Contacto";
-
-import Admin from "./components/Pages/Admin";
-
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <NavReactB />
-        <Switch>
-          <Route path="/" exact>
-            <Carro />
-
-            <SCards />
-
-            <Spublicidad />
-{/* 
-            <Sabout /> */}
-          </Route>
-
-          <Route path="/productos" exact>
-            <Productos />
-          </Route>
-          <Route path="/contacto" exact>
-            <Contacto />
-          </Route>
-          <Route path="/carrito" exact></Route>
-        </Switch>
-      </Router>
-
-      <Footer />
->>>>>>> e647b34933ff9948a3418d7413870201e6b1315a
     </div>
   );
 }
