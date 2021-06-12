24eb9663c3a75e649eabd037cb77bda28d907dde
Fabio <fabioramosnic@gmail.com>
2021-06-12 17:13:50 -0300

    Cambios Cony

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Carro from "./components/Carro";
@@ -6,24 +6,57 @@ import NavReactB from "./components/NavReactB";
 import SCards from "./components/SCards";
 import Spublicidad from "./components/Spublicidad";
 import Footer from "./components/Footer";
-import {BrowserRouter as Router, Redirect, Route, Switch, } from "react-router-dom";
-import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  NavLink,
+  Route,
+  Switch,
+} from "react-router-dom";
+import { useEffect, useState } from "react";
 import Productos from "./components/Pages/Productos";
 import Contacto from "./components/Contacto";
 import Admin from "./components/Pages/Admin";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-import Perfil from './components/Pages/Perfil'
-import PIndividual from './components/Pages/PIndividual';
+import Perfil from "./components/Pages/Perfil";
+import PIndividual from "./components/Pages/PIndividual";
 import AgregadoProducto from "./components/AgregadoProducto";
+import axios from "axios";
+import { Nav } from "react-bootstrap";
+import ScrollToTop from "./components/ScrollToTop";
 
-function App() {
-  const [user, setUser] = useState("");
-  const [token, setToken] = useState("");
+const localToken = JSON.parse(localStorage.getItem("token"))?.token || "";
+
+export default function App() {
+  const [user, setUser] = useState({});
+  const [token, setToken] = useState(localToken); //cuando no tenemos un token generado la const Token es un string vacio.
+  //llama a axios con el token del usuario, con useEffect reemplaza a un condicional, sólo se va a ejecutar cuando el token cambie de valor
+  useEffect(() => {
+    if (token) {
+      const request = async () => {
+        axios.defaults.headers = { "x-auth-token": token }; //> en el midleware/auth está definido el header que va a ser cabecera
+        const { data } = await axios.get("/auth"); //trae los datos del usuario loguedo mediante axios, y los guarda en data
+        setUser(data); //coloca los datos del usuario en la variable user
+      };
+      request(); //realiza el pedido
+    }
+  }, [token]); //se pone "token" como parámetro para que llame a useEffect cada vez que cambie
+
+  const logout = () => {
+    localStorage.removeItem("token"); //elimina el token
+    axios.defaults.headers = { "x-auth-token": "" };
+    setUser({}); //limpia el usuario
+    setToken(""); //limpia el token
+  };
+
   return (
     <div className="App">
       <Router>
-        <NavReactB />
+        <Route>
+          <ScrollToTop></ScrollToTop>
+        </Route>
+        <NavReactB userName={user.nombre} logout={logout} />
         <Switch>
           <Route path="/" exact>
             <Carro />
@@ -31,7 +64,6 @@ function App() {
             <SCards />
 
             <Spublicidad />
-
           </Route>
 
           <Route path="/productos" exact>
@@ -59,9 +91,8 @@ function App() {
           </Route>
 
           <Route path="/register">
-            
             <Register setToken={setToken} />
-            </Route>
+          </Route>
 
           <Route path="/">404</Route>
 
@@ -74,4 +105,4 @@ function App() {
   );
 }
 
-export default App;
+// export default App;
