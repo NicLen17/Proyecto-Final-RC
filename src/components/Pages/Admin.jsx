7d80e3c590cc501c1ab53afbeee26421b74aa46b
lrlichardi <lrlichardi@hotmail.com>
2021-06-15 21:47:58 -0300

    corrijo admin

--- src/components/Pages/Admin.jsx
+++ src/components/Pages/Admin.jsx
@@ -3,13 +3,15 @@ import axios from "axios";
 import { Tabs, Tab, Table } from 'react-bootstrap'
 import AgregadoProducto from '../AgregadoProducto'
 import './Admin.css'
+import Profile from "./Perfil";
+import Register from "./Productos";
+import Contacto from "../Contacto";
 
 function Admin() {
     const [products, setProducts] = useState([])
     const [mensajes, setMensajes] = useState([])
-    console.log(products);
-    console.log(mensajes);
-
+    const [lusers, setLusers] = useState([]);
+    
     useEffect(() => {
         const productos = async () => {
             const { data } = await axios.get('/productos');
@@ -19,10 +21,34 @@ function Admin() {
             const { data } = await axios.get('/mensajes')
             setMensajes(data)
         }
+
+        const getListaUsuarios = async () => {
+            const { data } = await axios.get("usuarios");
+            setLusers(data);
+          };
         productos();
         mensajes();
-    }, []);
+        getListaUsuarios();
 
+             
+          
+    }, []);
+    
+
+    
+    const handleSubmit_activo = async (event) => {
+      // const [usersactual, setUsersactual] = useState([]);
+  
+      // useEffect(() => {
+      //   const getUsuarioactual = async () => {
+      //     const { data } = await axios.get("usuarios");
+      //     setUsersactual(data);
+      //   };
+  
+      //   getUsuarioactual();
+      // }, []);
+      console.log("Deshabilitar/HAbilitar Usuario");
+    };
 
     return (
         <div>
@@ -62,42 +88,37 @@ function Admin() {
                     </Tab>
                     <Tab eventKey="profile" title="Usuarios">
                         <div>
-                            <Table striped bordered hover variant="dark">
-                                <thead>
-                                    <tr>
-                                        <th>Nombre</th>
-                                        <th>Sexo</th>
-                                        <th>Email</th>
-                                        <th>Ingreso</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>Pedro</td>
-                                        <td>Mark</td>
-                                        <td>Otto</td>
-                                        <td>@mdo</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2</td>
-                                        <td>Jacob</td>
-                                        <td>Thornton</td>
-                                        <td>@fat</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2</td>
-                                        <td>Jacob</td>
-                                        <td>Thornton</td>
-                                        <td>@fat</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2</td>
-                                        <td>Jacob</td>
-                                        <td>Thornton</td>
-                                        <td>@fat</td>
-                                    </tr>
-                                </tbody>
-                            </Table>
+                        <Table striped bordered hover variant="dark">
+              <thead>
+                <tr>
+                  <th>Nombre</th>
+                  <th>Teléfono</th>
+                  <th>Email</th>
+                  <th>Funciones</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {lusers.map((usuarios) => (
+                  <tr>
+                    <td>{usuarios.nombre}</td>
+                    <td>{usuarios.celular}</td>
+                    <td>{usuarios.email}</td>
+                    <td>
+                      <button
+                        onClick={handleSubmit_activo}
+                        type="button"
+                        class="btn btn-primary"
+                        data-bs-toggle="modal"
+                        data-bs-target="#exampleModal"
+                      >
+                        Habilitar/Deshab.
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
                         </div>
                     </Tab>
                     <Tab eventKey="contact" title="Mensajeria">
