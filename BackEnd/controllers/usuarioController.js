fef18345de44c35dab54c23297449f23d08a95f4
lrlichardi <lrlichardi@hotmail.com>
2021-06-22 22:16:11 -0300

    ultimos retoques

--- BackEnd/controllers/usuarioController.js
+++ BackEnd/controllers/usuarioController.js
@@ -37,11 +37,8 @@ exports.crearUsuario = async (req, res) => {
 };
 
 exports.obtenerUsuarios = async (req, res) => {
-    console.log('funcion obtener usuarios');
     try {
         const users = await Usuario.find();
-        // await Usuario.send({ nombre });
-        // console.log('🚀 ~ file: usuarioController.js ~ line 43 ~ exports.getUsers= ~ users', users);
         res.send(users);
     } catch (error) {
         res.status(400).json({ msg: 'error al obtener el usuarios' });
@@ -63,20 +60,15 @@ exports.stateUser = async (req, res) => {
     try {
         const userId = req.params.id;
         const usuario = await Usuario.findById(userId).select('-password -__v');
-        console.log(
-            '🚀 ~ file: usuarioController.js ~ line 67 ~ exports.stateUser= ~ usuario.estado',
-            usuario.estado
-        );
         if (usuario.estado) {
             req.body.estado = 'false';
         } else {
             req.body.estado = 'true';
         }
-
         const updatedUser = await Usuario.findByIdAndUpdate(userId, req.body, { new: true });
-        res.send(stateUser);
+        res.send(updatedUser);
     } catch (error) {
-        res.status(400).send({ msg: 'Hubo un error al actualizar el Usuario' });
+            res.status(400).send({ msg: 'Hubo un error al actualizar el Usuario' });
     }
 };
 
--- BackEnd/middlewares/authMiddleware.js
+++ BackEnd/middlewares/authMiddleware.js
@@ -1,24 +1,19 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
-    // Leer token
+     // Leer token
     const token = req.header('x-auth-token');
     // Revisar Token
     if (!token) {
-        console.log('🚀 ~ file: authMiddleware.js ~ line 8 ~ token', token);
-        return res.status(401).json({ msg: 'No hay Token, permiso no valido' });
-        if (window.confirm('No se puede ingresar a la siguiente direccion, desea volver a inicio?')) {
-            history.push('/');
-        }
+          return res.status(401).json({ msg: 'No hay Token, permiso no valido' });     
     }
     // Validar Token
     try {
         const cifrado = jwt.verify(token, process.env.SECRETA);
         req.usuario = cifrado.usuario;
-        console.log('🚀 ~ file: authMiddleware.js ~ line 14 ~ req.usuario', req.usuario);
         //Continuar al siguiente middleware
         return next();
     } catch (error) {
-        res.status(401).json({ msg: 'Token no valido' });
+        res.status(401).send({ msg: 'Token no valido' });
     }
 };
