ca25c63d5cd68c9e91d9d8e214d2143b43bf8680
Fabio <fabioramosnic@gmail.com>
2021-06-23 19:50:26 -0300

    Solucion mail

--- BackEnd/validations/mensajesValidation.js
+++ BackEnd/validations/mensajesValidation.js
@@ -3,6 +3,6 @@ const { check } = require('express-validator');
 exports.createMensajes = [
     check('nombreyapellido', 'El nombre y el apellido es obligatorio').not().isEmpty().isLength({ min: 6 , max:35 }),
     check('tel', 'El telefono es obligatorio').not().isEmpty().isNumeric().isLength({min:10}),
-    check('email', 'El Email es obligatorio').not().isEmpty().isEmail().isLength({ min: 25 , max:50 }),
+    check('email', 'El Email es obligatorio').not().isEmpty().isEmail(),
     check('mensaje', 'El mensaje es obligatoria y minimo 20 letras').not().isEmpty().isLength({ min: 20 , max:300 }),
         ];
